80f8625455cc621d98af0c022f5ae7d6549dbe19
Eleanor Murguia <eleanormurguia@gmail.com>
2017-07-06 16:51:00 -0700

    Onboarding basics with UIPageViewController implemented

--- BroadwayLotteryApp/BroadwayLotteryApp/AppDelegate.swift
+++ BroadwayLotteryApp/BroadwayLotteryApp/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //show welcome view controller 
+        let storyboard = UIStoryboard(name: "Welcome", bundle: nil)
+        let initialViewController = storyboard.instantiateInitialViewController()
+        
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
