74138ec02b936b037a2d6ee14b1bde99bcf3ef3a
Eleanor Murguia <eleanormurguia@gmail.com>
2017-07-06 15:06:09 -0700

    Project Setup

---
+++ BroadwayLotteryApp/BroadwayLotteryApp/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BroadwayLotteryApp
+//
+//  Created by Eleanor Murguia on 7/6/17.
+//  Copyright © 2017 Eleanor Murguia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
