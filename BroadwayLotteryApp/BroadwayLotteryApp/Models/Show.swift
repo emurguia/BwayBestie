b1f60b74e415fb829cc10611d814c044798d1fb6
Eleanor Murguia <eleanormurguia@gmail.com>
2017-07-11 16:43:46 -0700

    set up Lottery Home table view

---
+++ BroadwayLotteryApp/BroadwayLotteryApp/Models/Show.swift
@@ -0,0 +1,26 @@
+//
+//  Show.swift
+//  BroadwayLotteryApp
+//
+//  Created by Eleanor Murguia on 7/11/17.
+//  Copyright © 2017 Eleanor Murguia. All rights reserved.
+//
+
+import Foundation
+
+class Show{
+    
+    //MARK: Properties 
+    let title: String
+    let lotteryOpen: String
+    let lotteryClose: String
+   // let lotteryCloseEvening: String
+    
+    init(title: String, lotteryOpen: String, lotteryClose: String) {
+        self.title = title
+        self.lotteryOpen = lotteryOpen
+        self.lotteryClose = lotteryClose
+    }
+    
+
+}
