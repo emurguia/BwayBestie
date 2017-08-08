3cf4479bf4fa217405ac0840b1bf80ad575aefc5
Eleanor Murguia <eleanormurguia@gmail.com>
2017-08-08 11:04:17 -0700

    add button to bottom of onboarding page view

--- BroadwayLotteryApp/BroadwayLotteryApp/WelcomeMainViewController.swift
+++ BroadwayLotteryApp/BroadwayLotteryApp/WelcomeMainViewController.swift
@@ -1,45 +1,58 @@
 //
-//  WelcomePageViewController.swift
+//  WelcomeMainViewController.swift
 //  BroadwayLotteryApp
 //
-//  Created by Eleanor Murguia on 7/6/17.
+//  Created by Eleanor Murguia on 8/8/17.
 //  Copyright © 2017 Eleanor Murguia. All rights reserved.
 //
 
 import UIKit
 
-class WelcomePageViewController: UIPageViewController {
-
-    //get view controllers
-    private(set) lazy var orderedViewControllers: [UIViewController] = { return [self.newViewController(number:"One"), self.newViewController(number:"Two"), self.newViewController(number:"Three"), self.newViewController(number:"Four")]}()
+class WelcomeMainViewController: UIViewController{
+    
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    private(set) lazy var orderedViewControllers: [UIViewController] = { return [self.newViewController(number:"One"), self.newViewController(number:"Two"), self.newViewController(number:"Three")]}()
     
     private func newViewController(number: String) -> UIViewController{
         return UIStoryboard(name: "Welcome", bundle: nil).instantiateViewController(withIdentifier: "Welcome\(number)ViewController")
     }
     
+    var pageViewController: UIPageViewController = UIPageViewController.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: [:])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataSource = self
         
-        //set first view controller 
+        //style button
+        getStartedButton.layer.cornerRadius = 4
+
+        //load the page view controller and set first view controller
+        pageViewController.dataSource = self
+        pageViewController.view.backgroundColor = UIColor(red:0.67, green:0.00, blue:0.00, alpha:1.0)
         if let firstViewController = orderedViewControllers.first{
-            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+            self.pageViewController.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
+        
+        //change page view height to show get started button
+        pageViewController.view.frame = CGRect(x: 0,y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height - 75)
+        self.view.addSubview(pageViewController.view)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-   
 
+    @IBAction func getStartedButtonPressed(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "CollectUserInfo", bundle: nil)
+        let initialViewController = storyboard.instantiateInitialViewController()
+        
+        self.view.window?.rootViewController = initialViewController
+        self.view.window?.makeKeyAndVisible()
+    }
 }
 
-//MARK: UIPageViewControllerDataSource 
+//MARK: UIPageViewControllerDataSource
 
-extension WelcomePageViewController: UIPageViewControllerDataSource{
+extension WelcomeMainViewController: UIPageViewControllerDataSource{
     /*
-     * Required methods for UIPageViewControllerDataSource 
+     * Required methods for UIPageViewControllerDataSource
      * Methods provide view controllers to UIPageViewController
      */
     
@@ -90,7 +103,7 @@ extension WelcomePageViewController: UIPageViewControllerDataSource{
     }
     
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else{
+        guard let firstViewController = pageViewController.viewControllers?.first, let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else{
             return 0
         }
         return firstViewControllerIndex
