e1821fa6da448224a364b914d52985118e801825
Eleanor Murguia <eleanormurguia@gmail.com>
2017-08-09 16:03:36 -0700

    updated icon design

---
+++ BroadwayLotteryApp/BroadwayLotteryApp/Controllers/WelcomePageThreeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WelcomePageThreeViewController.swift
+//  BroadwayLotteryApp
+//
+//  Created by Eleanor Murguia on 8/9/17.
+//  Copyright © 2017 Eleanor Murguia. All rights reserved.
+//
+
+import Foundation
