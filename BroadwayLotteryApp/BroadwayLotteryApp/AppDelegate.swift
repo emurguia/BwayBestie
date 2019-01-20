3fc19c3fbed2823ad2e75def82dc0f11dfe53b66
Eleanor Murguia <eleanormurguia@gmail.com>
2019-01-19 16:47:10 -0800

    pod fix and title spacing

--- BroadwayLotteryApp/BroadwayLotteryApp/AppDelegate.swift
+++ BroadwayLotteryApp/BroadwayLotteryApp/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
+//import IQKeyboardManagerSwift
 import UserNotifications
 import SideMenuController
 
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-        IQKeyboardManager.sharedManager().enable = true
+//        IQKeyboardManager.sharedManager().enable = true
         
         //set up initial view controller
         let defaults = UserDefaults.standard
